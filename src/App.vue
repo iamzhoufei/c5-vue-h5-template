84a727024eefe4e396ffc3da2f2d9c1d5d754bad
秦锋 <iamzhoufei@gmail.com>
2023-01-08 03:46:34 +0800

    init: first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <RouterView />
+    <div btn px-4 py-2 icon-btn>
+      123
+    </div>
+    <TheFooter />
+  </main>
+</template>
